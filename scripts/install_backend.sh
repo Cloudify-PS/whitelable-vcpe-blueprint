34260a1f4db1737bba46a3d67d0f3f671fb895f0
Michael Shnizer <michaels@gigaspaces.com>
2017-04-26 17:33:32 +0300

    first commit

---
+++ scripts/install_backend.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+cd /opt/
+
+yum -y install wget unzip
+
+wget ${scp_backend_zip}
+
+unzip
+
+cd partner_backend
+
+./install_backend.sh
+
+# edit configuratuion
+
+
+./prepare_backend.sh
---
+++ scripts/install_ui.sh
@@ -0,0 +1,6 @@
+#!/bash
+cd /opt
+
+yum -y install wget unzip
+
+wget ${scp_ui_zip}
