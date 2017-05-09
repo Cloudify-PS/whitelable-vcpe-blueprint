8fb46f369e54f7bfbb99cb1d364889545029207b
Michael Shnizer <michaels@gigaspaces.com>
2017-05-09 14:49:16 +0300

    commit

--- scripts/install_backend.sh
+++ scripts/install_backend.sh
@@ -2,6 +2,9 @@
 
 cd /opt/
 
+
+sudo yum install epel-release
+
 sudo yum -y install wget unzip
 
 sudo yum -y install python-gunicorn
@@ -10,13 +13,20 @@ sudo wget ${scp_backend_zip}
 
 sudo unzip *.zip
 
-cd partner_backend
+rm -rf *.zip
+
+cd partner-backend-1.0
 
 sudo ./install_backend.sh
 
 # edit configuratuion
 
-# sudo ./prepare_backend.sh
+sudo sed -i /home/cspbackend/postgresql.cfg -e "/MANAGER/s/185.98.148.216/172.16.0.3/"
+
+sudo sed -i /home/cspbackend/postgresql.cfg -e "/MOCK_CLOUDIFY/s/True/False/"
+
+
+sudo ./prepare_backend.sh
 
 
 # MOCK_RMS=True
