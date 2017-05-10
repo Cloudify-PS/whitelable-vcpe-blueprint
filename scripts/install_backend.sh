d0a12b6a325ec96acfd9613a843282c7d621f40f
Michael Shnizer <michaels@gigaspaces.com>
2017-05-10 16:00:26 +0300

    commit

--- scripts/install_backend.sh
+++ scripts/install_backend.sh
@@ -24,11 +24,11 @@ sudo ./install_backend.sh
 
 # edit configuratuion
 
-sudo sed -i /home/cspbackend/postgresql.cfg -e "/MANAGER/s/185.98.148.216/172.16.0.3/"
+sudo sed -i /home/cspbackend/postgresql.cfg -e "/MANAGER/s/185.98.148.216/${manager_ip}/"
 
 sudo sed -i /home/cspbackend/postgresql.cfg -e "/MOCK_CLOUDIFY/s/True/False/"
 
-sudo sed -i /home/cspbackend/postgresql.cfg -e "/MANAGER_PASSWORD/s/Day21NB5ILWy/nEQSwrbNPz1w/"
+sudo sed -i /home/cspbackend/postgresql.cfg -e "/MANAGER_PASSWORD/s/Day21NB5ILWy/${manager_password}/"
 
 sudo ./prepare_backend.sh
 
