5d8b5e14cea1df470fb497de046556cbf175969d
Michael Shnizer <michaels@gigaspaces.com>
2017-05-24 16:53:32 +0300

    Update install_backend.sh

--- scripts/install_backend.sh
+++ scripts/install_backend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+ctx logger "Installing OS software"
 
 sudo yum -y install epel-release
 
@@ -11,18 +11,23 @@ sudo yum -y install python-gunicorn
 
 cd /opt/
 
+ctx logger "Downloading Backend"
+
 sudo wget ${scp_backend_zip}
 
 sudo unzip *.zip
 
 rm -rf *.zip
 
+ctx logger "installing Backend"
 
 cd partner-backend-1.0
 
 sudo ./install_backend.sh
 
 # edit configuratuion
+
+ctx logger "Preparing BackEnd"
 
 sudo sed -i /home/cspbackend/postgresql.cfg -e "/MANAGER/s/185.98.148.216/${manager_ip}/"
 
@@ -32,9 +37,11 @@ sudo sed -i /home/cspbackend/postgresql.cfg -e "/MANAGER_PASSWORD/s/Day21NB5ILWy
 
 sudo ./prepare_backend.sh
 
+ctx logger "Populating Example Customers"
+
 cd /opt/partner-backend-1.0/load-tests
 
-sudo python load_test.py
+sudo python demo_test.py
 
 # MOCK_RMS=True
 #
